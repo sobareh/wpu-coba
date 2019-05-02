132a4f65c74502d35e7d0670558e87cb3621c74d
Sobareh <ahmadsobarry@gmail.com>
2019-05-02 23:43:33 +0700

    Membuat goals.php dan menghilangkan setting proxy pada settings.json

---
+++ goals.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mimpi</title>
+</head>
+<body>
+    <h1>Saya akan menjadi Fullstack Developer di 2020 Januari</h1>
+    <p>Kenapa Menjadi Programmer? <br> itu karena saya ingin menjadi seseorang yang bermanfaat dalam kapasitas saya sebagai seorang teknisi dibalik alat2/aplikasi yang saya ciptakan.</p>
+</body>
+</html>
